b9bf8eae5c12490f90d118d5e6c6a5a66113a13c
Erik Thalén <erik.thalen@hyperisland.se>
2026-03-21 20:04:12 +0100

    fix links in pages?

--- packages/docs/vite.ssg.config.ts
+++ packages/docs/vite.ssg.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  define: {
+    "process.env.BASE_URL": JSON.stringify(process.env.BASE_URL ?? "/"),
+  },
   build: {
     ssr: "src/index.ts",
     outDir: "dist/.server",
