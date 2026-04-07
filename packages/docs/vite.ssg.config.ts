70c474385e5b641740d7c3d8e51a69bccf0bcc26
Erik Thalén <erik.thalen@hyperisland.se>
2026-04-07 13:20:12 +0200

    fix deploy github action

--- packages/docs/vite.ssg.config.ts
+++ packages/docs/vite.ssg.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
   define: {
     "process.env.BASE_URL": JSON.stringify(process.env.BASE_URL ?? "/"),
+    __TABLER_ICONS_DIR__: JSON.stringify(
+      resolve(import.meta.dirname, "node_modules/@tabler/icons/icons"),
+    ),
   },
   build: {
     ssr: "src/index.ts",
